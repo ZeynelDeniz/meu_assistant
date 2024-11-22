3d3b6c8352b2a37f3551d5c7c77a91f63ffc2f5d
CanKankaya <can.kankaya@outlook.com>
2024-11-22 20:43:18 +0300

    .

--- lib/models/map_location.dart
+++ lib/models/map_location.dart
@@ -3,13 +3,13 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class MapLocation {
   final String name;
   final LatLng position;
-  final String? phone;
+  final List<String?>? phones;
   final String? email;
 
   MapLocation({
     required this.name,
     required this.position,
-    this.phone,
+    this.phones,
     this.email,
   });
 }
